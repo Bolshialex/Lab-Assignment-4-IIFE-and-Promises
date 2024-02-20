3a1fe4bcb4829dbbf2ecbedcd2f3421acc8a4dcc
bolshialex <bolshialex000@gmail.com>
2024-02-19 20:34:33 -0800

    Add code for task 3 and task 4

--- main.js
+++ main.js
@@ -1,6 +1,9 @@
 (function () {
     let studentDataTask1 = [];
-
+    let studentInfoTask2 = [];
+    let greaterAgeBtn = document.getElementById('greaterThan');
+    let avgAgeBtn = document.getElementById('avgAge');
+    let oddIndex = document.getElementById('oddIndex');
     
     fetch('students.json')
         .then(function(response){
@@ -25,7 +28,7 @@
         })
 
 
-    let studentInfoTask2 = [];
+    
 
     function fetchData(){
         
@@ -58,6 +61,48 @@
         console.error("Error fetching data", error);
         alert('Error fetching data after calling the fetchData function');
     })
-    
+
+
+    greaterAgeBtn.addEventListener('click', () => {
+        let resultPlacement = document.getElementById('results');
+        const ulElement = document.createElement('ul');
+        
+        if(resultPlacement.firstElementChild){
+        }
+        else{
+        
+            resultPlacement.appendChild(ulElement);
+
+            const filteredAge = studentDataTask1.filter(students => students.age > 20);
+
+            filteredAge.forEach(student => {
+                const liElement = document.createElement('li');
+                ulElement.appendChild(liElement).innerText += `Student: ${student.name} | Age: ${student.age} | Major: ${student.major} | Grade: ${student.grade}\n`;
+            });
+        }
+    })
+
+    avgAgeBtn.addEventListener('click', () => {
+        let studentAgeSum = 0;
+        studentDataTask1.forEach(student => {
+            studentAgeSum += student.age;
+        })
+
+        let avgAge = studentAgeSum / (studentDataTask1.length);
+        
+        //clear the box and place the avg age into a message
+    })
+
+    oddIndex.addEventListener('click', () => {
+        let resultPlacement = document.getElementById('results');
+        const ulElement = document.createElement('ul');
+        resultPlacement.appendChild(ulElement);
+
+        for(let i = 1; i<studentDataTask1.length; i+=2){
+            const liElement = document.createElement('li');
+            ulElement.appendChild(liElement).innerText += `Student: ${studentDataTask1[i].name} | Age: ${studentDataTask1[i].age} | Major: ${studentDataTask1[i].major} | Grade: ${studentDataTask1[i].grade}\n`;
+        }
+
+    })
 
 })();
